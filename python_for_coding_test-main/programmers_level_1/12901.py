53f14dc9df1e4f738325e583d6099291a4cf022c
ChoiJaeHoon <zpqls321@naver.com>
2021-05-02 01:30:55 +0900

    solved 12901

---
+++ python_for_coding_test-main/programmers_level_1/12901.py
@@ -0,0 +1,9 @@
+def solution(a, b):
+    answer = ''
+    month=[31,29,31,30,31,30,31,31,30,31,30,31]
+    day=['FRI','SAT','SUN','MON','TUE','WED','THU']
+    tmp=0
+    for i in range(a-1):
+        tmp+=month[i]
+    answer=day[(tmp+b)%7-1]
+    return answer
